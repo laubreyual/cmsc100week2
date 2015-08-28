8cb6818c4ac3b9e69acf6006d3c5bf48827a9218
laubsofficial <laubreyual@gmail.com>
2015-08-28 11:37:50 +0800

    basic nodejs

---
+++ controllers/teacher.js
@@ -0,0 +1,12 @@
+exports.find = function(req, res) {
+    res.send('Find a teacher');
+};
+exports.create = function(req, res) {
+    res.send('Create a teacher');
+};
+exports.update = function(req, res) {
+    res.send('Update a teacher');
+};
+exports.delete = function(req, res) {
+    res.send('Delete a teacher');
+};
