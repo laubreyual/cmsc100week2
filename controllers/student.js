4e51cead46ac512386d22e758404229b3e9abf9f
laubsofficial <cyber2103@gmail.com>
2015-09-04 12:37:26 +0800

    combined nodejs with mysql and tried the CRUD operations

--- controllers/student.js
+++ controllers/student.js
@@ -1,3 +1,42 @@
-exports.find = function(req, res) {
-    res.send('Hello World!');
+var db = require(__dirname + '/../lib/mysql');
+
+exports.find = function(req, res, next) {
+	db.query('SELECT * FROM student',
+		function(err, rows){
+			if(err) return next(err);
+		    res.send(rows);
+		});
+};
+
+exports.insert = function(req, res, next) {
+	db.query('INSERT INTO student VALUES(?,?,?)', [req.body.studNo, req.body.name, req.body.bdate],
+		function(err, rows){
+			if(err) return next(err);
+		  	res.send(rows);
+		});
+};
+
+exports.findOne = function(req, res, next) {
+	db.query('SELECT * FROM student WHERE studNo=?', [req.params.id],
+		function(err, rows){
+			if(err) return next(err);
+			if(rows.length === 0) res.status(404).send("Student Not Found!");
+			else res.send(rows[0]);
+		});
+};
+
+exports.update = function(req, res, next) {
+	db.query('UPDATE student SET ? WHERE studno=?', [req.body, req.params.id],
+		function(err, rows){
+			if(err) return next(err);		
+			res.send(rows);
+		});
+};
+
+exports.remove = function(req, res, next) {
+	db.query('DELETE FROM student WHERE studno=?', [req.params.id],
+		function(err, rows){
+			if(err) return next(err);		
+			res.send(rows);
+		});
 };
